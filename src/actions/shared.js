906795fa5501bcaac3a69932354fcb51f877aeda
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-15 18:23:11 +0200

    Initial data after mounting app component

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,8 +4,8 @@ import {receiveQuestions} from './questions'
 
 export function handleInitialData() {
   return (dispatch) => {
-    return getInitialData().then((users, questions) => {
-      dispatch(receiveUsers(users)),
+    return getInitialData().then(({users, questions}) => {
+      dispatch(receiveUsers(users))
       dispatch(receiveQuestions(questions))
     })
   }
