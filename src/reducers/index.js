9a6ea1ab3b35fa9b27a7cf83677292f8e066685f
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-15 18:03:20 +0200

    Clean up and add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux'
+import loggedUser from './loggedUser'
+import users from './users'
+import questions from './questions'
+
+export default combineReducers({loggedUser, users, questions})
