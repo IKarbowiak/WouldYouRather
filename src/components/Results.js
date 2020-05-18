292ffbed47f90825a0c926b8aeac746b3afb7c24
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-18 20:00:33 +0200

    Add percent result info

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,21 +9,52 @@ class Results extends Component {
     const optionOneVotes = question.optionOne.votes
     const optionTwoVotes = question.optionTwo.votes
     const numOfVotes = optionOneVotes.length + optionTwoVotes.length
+    const optionOnePercent = Math.round((optionOneVotes.length / numOfVotes) * 100)
+    const optionTwoPercent = Math.round((optionTwoVotes.length / numOfVotes) * 100)
 
     return (
       <Card.Body className="card-block px-2">
-        <h4>Results</h4>
+        <h4 style={{textAlign: "center"}}>Results</h4>
+        <h6>Would you rather...</h6>
         <div className={`alert ${optionOneVotes.includes(loggedUser) ? 'alert-success' : 'border border-secondary'}`}>
-        <p>
-            Would you rather {question.optionOne.text}?
+          <p>
+              ...{question.optionOne.text}?
           </p>
-          {optionOneVotes.length} out of {numOfVotes}
+          <div className="progress">
+            <div
+              className="progress-bar bg-success"
+              role="progressbar"
+              style={{width: `${optionOnePercent}%`}}
+              aria-valuenow={optionOnePercent}
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              {optionOnePercent}%
+            </div>
+          </div>
+          <div style={{textAlign: "center"}}>
+            {optionOneVotes.length} out of {numOfVotes}
+          </div>
         </div>
         <div className={`alert ${optionTwoVotes.includes(loggedUser) ? 'alert-success' : 'border border-secondary'}`}>
-        <p>
-            Would you rather {question.optionTwo.text}?
+          <p>
+            ...{question.optionTwo.text}?
           </p>
-          {optionTwoVotes.length} out of {numOfVotes}
+          <div className="progress">
+            <div
+              className="progress-bar bg-secondary"
+              role="progressbar"
+              style={{width: `${optionTwoPercent}%`}}
+              aria-valuenow={optionTwoPercent}
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              {optionTwoPercent}%
+            </div>
+          </div>
+          <div style={{textAlign: "center"}}>
+            {optionTwoVotes.length} out of {numOfVotes}
+          </div>
         </div>
       </Card.Body>
     )
